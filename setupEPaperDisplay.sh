666378c5d449834f0f53986e6c7aaa8b41e0b058
David Weiss <davidweisss@gmail.com>
2018-10-18 11:17:24 +0200

    Update setupEPaperDisplay.sh

--- setupEPaperDisplay.sh
+++ setupEPaperDisplay.sh
@@ -7,3 +7,16 @@ cd bcm2835-1.57
 ./configure
 sudo make check
 sudo make install
+
+## Python libraries
+sudo apt-get install python-dev
+## Installing the RPi.GPIO package (GPIO interface functions). Copy the installation package RPi.GPIO to your RPi board, and unzip it. Enter the unzipped file under the terminal, and run the following command to install the library:
+sudo python setup.py install
+## Run the following command to install the library smbus (I2C interface functions)
+sudo apt-get install python-smbus
+## Run the following command to install the library serial, which contains UART interface functions
+sudo apt-get install python-serial
+## Installing the library spidev (SPI functions). Copy the installation package spidev to your RPi board, and unzip it. Enter the unzip file under the terminal, and run the following command to install the library:
+sudo python setup.py install
+## Run the following command to install the Python imaging library
+sudo apt-get install python-imaging
