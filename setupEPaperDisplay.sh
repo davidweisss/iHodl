6968e2cf17e4fa96f5c05da9d4856581e66fa810
David Weiss <davidweisss@mail.com>
2018-10-18 11:49:52 +0000

    setup

--- setupEPaperDisplay.sh
+++ setupEPaperDisplay.sh
@@ -16,13 +16,21 @@ wget https://files.pythonhosted.org/packages/e2/58/6e1b775606da6439fa3fd1550e7f7
 ## spidev
 wget https://files.pythonhosted.org/packages/36/83/73748b6e1819b57d8e1df8090200195cdae33aaa22a49a91ded16785eedd/spidev-3.2.tar.gz
 
+## extract
+tar xvzf RPi.GPIO-0.6.3.tar.gz
+tar xvzf spidev-3.2.tar.gz
+
 ## Installing the RPi.GPIO package (GPIO interface functions). Copy the installation package RPi.GPIO to your RPi board, and unzip it. Enter the unzipped file under the terminal, and run the following command to install the library:
+cd RPi.GPIO-0.6.3
 sudo python setup.py install
+cd ..
 ## Run the following command to install the library smbus (I2C interface functions)
-sudo apt-get install python-smbus
+sudo apt-get install python-smbus -y
 ## Run the following command to install the library serial, which contains UART interface functions
 sudo apt-get install python-serial
 ## Installing the library spidev (SPI functions). Copy the installation package spidev to your RPi board, and unzip it. Enter the unzip file under the terminal, and run the following command to install the library:
+cd spidev-3.2
 sudo python setup.py install
+cd ..
 ## Run the following command to install the Python imaging library
-sudo apt-get install python-imaging
+sudo apt-get install python-imaging -y
