34bc813f3239e9f7852371e24c824da67ad75935
David Weiss <davidweisss@mail.com>
2018-10-18 09:14:25 +0000

    bcm libraries

--- setupEPaperDisplay.sh
+++ setupEPaperDisplay.sh
@@ -1,1 +1,9 @@
-
+sudo apt-get install p7zip
+wget https://www.waveshare.com/w/upload/1/1c/2.13inch_e-Paper_HAT-code.7z
+p7zip -d 2.13inch_e-Paper_HAT-code.7z
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.57.tar.gz
+tar xvzf bcm2835-1.57.tar.gz
+cd bcm2835-1.57
+./configure
+sudo make check
+sudo make install
